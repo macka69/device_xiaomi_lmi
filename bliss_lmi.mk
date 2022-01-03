6689524032f06ac0c7a94bd13fa2f1feff37b09d
John Macka <macka2004@gmail.com>
2022-01-03 13:28:44 +1000

    lmi: blissify

--- bliss_lmi.mk
+++ bliss_lmi.mk
@@ -9,12 +9,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Inherit from lmi device
 $(call inherit-product, device/xiaomi/lmi/device.mk)
 
-PRODUCT_NAME := lineage_lmi
+# Boot animation resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := bliss_lmi
 PRODUCT_DEVICE := lmi
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
