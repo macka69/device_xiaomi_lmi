e80ebebc6ea6d4a4159c3bf6d5f41e47abd520fe
John Macka <macka2004@gmail.com>
2022-01-11 14:31:10 +1000

    lmi: import and switch to QTI Lights AIDL from CAF https://source.codeaurora.org/quic/la/device/qcom/vendor-common/tree/lights?h=LA.UM.9.12.1.r1-00300-SMxx50.QSSI12.0
    Co-authored-by: ZenkaBestia <z.mail.6600@gmail.com>

--- device.mk
+++ device.mk
@@ -285,7 +285,8 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.xiaomi_lmi
+    android.hardware.lights-service.qti \
+    lights.kona
 
 # Media
 PRODUCT_PACKAGES += \
